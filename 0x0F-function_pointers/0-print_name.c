c1a6f85efcc3b1a13d7456e1e9c5ab783d2f4eca
Thobekile97 <tndlovi56@gmail.com>
2023-03-22 13:19:55 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,20 @@
 #include "function_pointers.h"
-	#include <stdio.h>
-	/**
-	 * print_name - print name using pointer to function
-	 * @name: string to add
-	 * @f: pointer to function
-	 * Return: nothing
-	 **/
-	void print_name(char *name, void (*f)(char *))
-	{
-		if (name == NULL || f == NULL)
-			return;
+#include <stdio.h>
 
+/**
+ * print_name - prints a name using a given function
+ * @name: the name to be printed
+ * @f: a pointer to a function that takes a character pointer as an argument
+ *
+ * Description: This function takes a name and a function pointer as arguments.
+ *              It then calls the function pointer, passing in the name argument.
+ *              This is a way to allow the caller to specify how the name should
+ *              be printed.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name == NULL || f == NULL)
+        return;
 
-		f(name);
-	}
+    f(name);
+}
