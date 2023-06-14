f93216a235ae7a552933b5ec74a5e72d48e1a0b1
Thobekile97 <tndlovi56@gmail.com>
2023-06-14 13:32:02 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,23 +1,41 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-/**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: creat array of size size and assign char c
- * Return: pointer to array, NULL if fail
- *
- */
-char *create_array(unsigned int size, char c)
-{
-	char *str;
-	unsigned int i;
 
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
-		return (NULL);
+char *create_array(unsigned int size, char c) {
+    if (size == 0) {
+        return NULL;  // Return NULL if size = 0
+    }
+    
+    char *array = (char *)malloc(size * sizeof(char));  // Allocate memory for the array
+    
+    if (array == NULL) {
+        return NULL;  // Return NULL if memory allocation fails
+    }
+    
+    for (unsigned int i = 0; i < size; i++) {
+        array[i] = c;  // Initialize each element with the specified char
+    }
+    
+    return array;  // Return a pointer to the array
+}
 
-	for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
+int main() {
+    unsigned int size = 10;
+    char c = 'A';
+    
+    char *array = create_array(size, c);
+    
+    if (array == NULL) {
+        printf("Failed to create the array.\n");
+    } else {
+        printf("Array created successfully:\n");
+        for (unsigned int i = 0; i < size; i++) {
+            printf("%c ", array[i]);
+        }
+        printf("\n");
+        
+        free(array);  // Remember to free the allocated memory
+    }
+    
+    return 0;
 }
