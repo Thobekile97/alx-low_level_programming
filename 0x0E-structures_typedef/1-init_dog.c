5de96d604d8543ba732b2d0c366cc1609858ed18
Thobekile97 <tndlovi56@gmail.com>
2023-06-19 14:42:53 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct dog {
     char *name;
@@ -7,22 +9,31 @@ struct dog {
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner) {
-    d->name = name;
+    d->name = malloc(strlen(name) + 1);
+    strcpy(d->name, name);
     d->age = age;
-    d->owner = owner;
+    d->owner = malloc(strlen(owner) + 1);
+    strcpy(d->owner, owner);
+}
+
+void free_dog(struct dog *d) {
+    free(d->name);
+    free(d->owner);
 }
 
 int main() {
     struct dog myDog;
-    char *name = "Max";
-    float age = 4.5;
-    char *owner = "John Doe";
+    char *name = "Buddy";
+    float age = 2.5;
+    char *owner = "Jane Smith";
 
     init_dog(&myDog, name, age, owner);
 
     printf("Dog's Name: %s\n", myDog.name);
     printf("Dog's Age: %.1f\n", myDog.age);
     printf("Dog's Owner: %s\n", myDog.owner);
+
+    free_dog(&myDog);
 
     return 0;
 }
