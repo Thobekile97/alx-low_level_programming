62594c1bbfea6a51bcd8419171d77241db1f564b
Thobekile97 <tndlovi56@gmail.com>
2023-06-21 14:33:23 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 
-void array_iterator(int *array, size_t size, void (*action)(int)) {
-    for (size_t i = 0; i < size; i++) {
-        action(array[i]);
-    }
-}
-
 void print_elem(int elem) {
-    printf("%d ", elem);
+    printf("%d\n", elem);
 }
 
-void print_elem_octal(int elem) {
-    printf("0%o ", elem);
+void print_elem_hex(int elem) {
+    printf("0x%x\n", elem);
 }
 
 int main(void) {
     int array[5] = {0, 98, 402, 1024, 4096};
     size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Printing array elements in decimal: ");
-    array_iterator(array, size, print_elem);
-    printf("\n");
-
-    printf("Printing array elements in octal: ");
-    array_iterator(array, size, print_elem_octal);
-    printf("\n");
+    array_iterator(array, size, &print_elem);
+    array_iterator(array, size, &print_elem_hex);
 
     return 0;
 }
