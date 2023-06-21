39e408e0479b12d51ff1d751556d344d858f824f
Thobekile97 <tndlovi56@gmail.com>
2023-06-21 13:46:03 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,18 +8,10 @@ void print_with_newline(char *name) {
     printf("%s\n", name);
 }
 
-void print_with_exclamation(char *name) {
-    printf("%s!\n", name);
-}
-
 int main() {
     char name[] = "John Doe";
 
-    printf("Printing name with newline:\n");
     print_name(name, print_with_newline);
-
-    printf("\nPrinting name with exclamation:\n");
-    print_name(name, print_with_exclamation);
 
     return 0;
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,30 @@
-#include "function_pointers.h"
-	#include <stdio.h>
-	/**
-	 * array_iterator - prints each array elem on a newl
-	 * @array: array
-	 * @size: how many elem to print
-	 * @action: pointer to print in regular or hex
-	 * Return: void
-	 */
-	void array_iterator(int *array, size_t size, void (*action)(int))
-	{
-		unsigned int i;
+#include <stdio.h>
 
+void array_iterator(int *array, size_t size, void (*action)(int)) {
+    for (size_t i = 0; i < size; i++) {
+        action(array[i]);
+    }
+}
 
-		if (array == NULL || action == NULL)
-			return;
+void print_element(int element) {
+    printf("%d ", element);
+}
 
+void square_element(int element) {
+    printf("%d ", element * element);
+}
 
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
-	}
+int main() {
+    int array[] = {1, 2, 3, 4, 5};
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Printing array elements: ");
+    array_iterator(array, size, print_element);
+    printf("\n");
+
+    printf("Printing squared array elements: ");
+    array_iterator(array, size, square_element);
+    printf("\n");
+
+    return 0;
+}
