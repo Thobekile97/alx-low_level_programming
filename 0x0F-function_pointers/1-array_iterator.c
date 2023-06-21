34e7877bd5733763d7e2046317f9cae969e6a433
Thobekile97 <tndlovi56@gmail.com>
2023-06-21 13:50:05 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,24 +6,24 @@ void array_iterator(int *array, size_t size, void (*action)(int)) {
     }
 }
 
-void print_element(int element) {
-    printf("%d ", element);
+void print_elem(int elem) {
+    printf("%d ", elem);
 }
 
-void square_element(int element) {
-    printf("%d ", element * element);
+void print_elem_octal(int elem) {
+    printf("0%o ", elem);
 }
 
-int main() {
-    int array[] = {1, 2, 3, 4, 5};
+int main(void) {
+    int array[5] = {0, 98, 402, 1024, 4096};
     size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Printing array elements: ");
-    array_iterator(array, size, print_element);
+    printf("Printing array elements in decimal: ");
+    array_iterator(array, size, print_elem);
     printf("\n");
 
-    printf("Printing squared array elements: ");
-    array_iterator(array, size, square_element);
+    printf("Printing array elements in octal: ");
+    array_iterator(array, size, print_elem_octal);
     printf("\n");
 
     return 0;
