2e4525627b605c56a5233e85cad21a09de75cd8b
Thobekile97 <tndlovi56@gmail.com>
2023-08-17 15:17:13 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,4 +1,3 @@
-#include <stddef.h>
 #include "lists.h"
 
 /**
@@ -9,12 +8,12 @@
 size_t dlistint_len(const dlistint_t *h)
 {
     size_t count = 0;
-    
+
     while (h != NULL)
     {
         h = h->next;
         count++;
     }
-    
+
     return count;
 }
